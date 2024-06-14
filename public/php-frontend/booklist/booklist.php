dfce9a0c37eaf0385b790e1f8602775732777d04
Charles <149758871+Luhartt@users.noreply.github.com>
2024-06-14 12:39:09 +0800

    filters for booklist, styles for booklist

--- public/php-frontend/booklist/booklist.php
+++ public/php-frontend/booklist/booklist.php
@@ -13,7 +13,68 @@
     <main>
         <!-- side-panel for filters -->
         <aside class="side-panel">
-            side
+            <h1>Filters</h1>
+
+            <div class="filters">
+                <!-- form for filters -->
+                <form action=#"">
+
+                    <h2>Search</h2>
+                    <select name="search-by" id="search-by">
+                        <option value="search">Author</option>
+                        <option value="search">Title</option>
+                    </select>
+                    <br>
+                    <input type="text" name="search" id="search">
+
+                    <h2>Status</h2>
+                    <label for="available"> <input type="radio" name="availability" id="available">Available</label>
+                    <label for="not-available"> <input type="radio" name="availability" id="not-available">Not Available</label>
+                    <label for="available-library"> <input type="radio" name="availability" id=available-library">Available in Library</label>
+
+                    <h2>Publishing Year</h2>
+                    <select name="publish-year" id="publish-year">
+                        <?php //loop options, distinct years in db
+
+                        //example loop only
+                        $i = 5;
+                        while ($i != 0) { ?>
+                            <option value="<?php echo $i //option value here
+                                            ?>"> <?php echo $i // option her
+                                                    ?> </option>
+
+                        <?php $i--;
+                        } ?>
+
+                    </select>
+
+                    <h2>Book Type</h2>
+                    <label for="academic"> <input type="radio" name="book-type" id="academic">Academic</label>
+                    <label for="fictional"> <input type="radio" name="book-type" id="fictional">Fictional</label>
+                    <label for="non-fictional"> <input type="radio" name="book-type" id="non-fictional">Non Fictional</label>
+
+                    <h2>Genre</h2>
+                    <select name="genre" id="genre">
+                        <?php //loop options, distinct years in db
+
+                        //example loop only
+                        $i = 5;
+                        while ($i != 0) { ?>
+                            <option value="<?php echo $i //option value here
+                                            ?>"> <?php echo $i // option her
+                                                    ?> </option>
+
+                        <?php $i--;
+                        } ?>
+
+                    </select>
+                    <br>
+
+                    <button> Submit </button>
+
+
+                </form>
+            </div>
         </aside>
         <!-- main section -->
         <section class="booklist-container">
