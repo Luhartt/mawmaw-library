42a08f7b40d47b9de84ad1d84a502291bf4649d7
Charles <149758871+Luhartt@users.noreply.github.com>
2024-06-18 12:44:40 +0800

    add form-containers, modify login.scss
    login.scss - finish containers, overflows done
    login.php - added form-containers, anchor links not yet finished

--- public/php-frontend/login/login.php
+++ public/php-frontend/login/login.php
@@ -18,122 +18,123 @@
 
             <!-- id lang may prefix na register/login para unique pagtinawag sa js, sa php namamn button ung irerefernce para alam ng script ano irurun -->
 
-
             <div class="register-container" id="register-container">
 
-                <form action="#" method="POST">
-
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>Create an account</p>
-                    <input type="text" name="username" id="register-username" placeholder="Username">
-                    <span class="material-symbols-outlined">person</span>
-
-                    <input type="text" name="name" id="register-name" placeholder="Name">
-                    <span class="material-symbols-outlined">
-                        edit_square
-                    </span>
-
-                    <input type="email" name="email" id="register-email" placeholder="Email">
-                    <span class="material-symbols-outlined">
-                        alternate_email
-                    </span>
-
-                    <input type="password" name="password" id="register-password" placeholder="Password">
-                    <span class="material-symbols-outlined">
-                        lock_open
-                    </span>
-                    <button type="button" name="register" id="register">Register</button>
-                    <p>already a member? <span><u>Click Here</u></span></p>
-
-                </form>
+                <div class="form-container">
+                    <form action="#" method="POST">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>Create an account</h2>
+                        <input type="text" name="username" id="register-username" placeholder="Username">
+                        <span class="material-symbols-outlined">person</span>
+                        <input type="text" name="name" id="register-name" placeholder="Name">
+                        <span class="material-symbols-outlined">
+                            edit_square
+                        </span>
+                        <input type="email" name="email" id="register-email" placeholder="Email">
+                        <span class="material-symbols-outlined">
+                            alternate_email
+                        </span>
+                        <input type="password" name="password" id="register-password" placeholder="Password">
+                        <span class="material-symbols-outlined">
+                            lock_open
+                        </span>
+                        <button type="button" name="register" id="register">Register</button>
+                        <p>already a member? <a href="#login-container">Click Here</a></p>
+                    </form>
+                </div>
 
 
-                <form action="" class="verify-otp">
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>the 4-figit code has been sent to your email</p>
-
-                    <div class="otp">
-                        <input type="text" name="otp1" id="register-otp1" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp2" id="register-otp2" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp3" id="register-otp3" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp4" id="register-otp4" maxlength="1" pattern="[0-9]{1}">
-                    </div>
-
-                    <button type="button" class="resend-otp"><u>Resend OTP</u></button>
-                    <button type="button">Verify</button>
-                </form>
+                <div class="form-container">
+                    <form action="" class="verify-otp">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>the 4-figit code has been sent to your email</h2>
+                        <div class="otp">
+                            <input type="text" name="otp1" id="register-otp1" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp2" id="register-otp2" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp3" id="register-otp3" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp4" id="register-otp4" maxlength="1" pattern="[0-9]{1}">
+                        </div>
+                        <button type="button" class="resend-otp"><u>Resend OTP</u></button>
+                        <button type="button">Verify</button>
+                    </form>
+                </div>
 
             </div>
 
             <div class="login-container" id="login-container">
 
-                <form action="">
-
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>Please enter your email and password</p>
-
-                    <input type="email" name="email" id="login-email" placeholder="Email">
-                    <span class="material-symbols-outlined">
-                        alternate_email
-                    </span>
-
-                    <input type="password" name="password" id="login-password">
-                    <span class="material-symbols-outlined">
-                        lock_open
-                    </span>
-                    <button type="button" name="login" id="login">Login</button>
-                    <p>Don't have an account? <span> <u>Sign Up</u> </span></p>
-                </form>
+                <div class="form-container">
+                    <form action="">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>Please enter your email and password</h2>
+                        <input type="email" name="email" id="login-email" placeholder="Email">
+                        <span class="material-symbols-outlined">
+                            alternate_email
+                        </span>
+                        <input type="password" name="password" id="login-password" placeholder = "Password">
+                        <span class="material-symbols-outlined">
+                            lock_open
+                        </span>
+                        <div class = "login-subcontainer">
+                            <label><input type="checkbox" name="remember" id="remember">&nbsp; Remember Me</label>
+                            <a href="#forgot-password-container">Forgot Password?</a>
+                        </div>
+                        <button type="button" name="login" id="login">Login</button>
+                        <p>Don't have an account? <a href="#register-container">Click Here</a></p>
+                    </form>
+                </div>
 
             </div>
 
             <div class="forgot-password-container" id="forgot-password-container">
 
 
-                <form action="#" class="forgot-email">
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>Please enter your email</p>
-                    <input type="email" name="email" id="forgot-email" placeholder="Email">
-                    <span class="material-symbols-outlined">
-                        alternate_email
-                    </span>
+                <div class="form-container">
+                    <form action="#" class="forgot-email">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>Please enter your email</h2>
+                        <input type="email" name="email" id="forgot-email" placeholder="Email">
+                        <span class="material-symbols-outlined">
+                            alternate_email
+                        </span>
+                        <button type="button">Find my email</button>
+                        <p>Don't have an account? <a href="#register-container">Click Here</a></p>
+                    </form>
+                </div>
+                <div class="form-container">
+                    <form action="" class="verify-otp">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>the 4-figit code has been sent to your email</h2>
+                        <div class="otp">
+                            <input type="text" name="otp1" id="forgot-otp1" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp2" id="forgot-otp2" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp3" id="forgot-otp3" maxlength="1" pattern="[0-9]{1}">
+                            <input type="text" name="otp4" id="forgot-otp4" maxlength="1" pattern="[0-9]{1}">
+                        </div>
+                        <button type="button" class="resend-otp"><u>Resend OTP</u></button>
+                        <button type="button">Verify</button>
+                        <p>Don't have an account? <a href="#register-container">Click Here</a></p>
+                    </form>
+                </div>
 
-                    <button type="button">Find my email</button>
-
-                </form>
-                <form action="" class="verify-otp">
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>the 4-figit code has been sent to your email</p>
-
-                    <div class="otp">
-                        <input type="text" name="otp1" id="forgot-otp1" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp2" id="forgot-otp2" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp3" id="forgot-otp3" maxlength="1" pattern="[0-9]{1}">
-                        <input type="text" name="otp4" id="forgot-otp4" maxlength="1" pattern="[0-9]{1}">
-                    </div>
-
-                    <button type="button" class="resend-otp"><u>Resend OTP</u></button>
-                    <button type="button">Verify</button>
-                    <p>Don't have an account? <span> <u>Sign Up</u> </span></p>
-                </form>
 
 
-
-                <form action="" class="change-password">
-                    <h1>MAWMAW LIBRARY</h1>
-                    <p>Please enter your new password</p>
-
-                    <input type="password" name="password" id="forgot-password">
-                    <span class="material-symbols-outlined">
-                        lock_open
-                    </span>
-                    <input type="password" name="confirm-password" id="forgot-confirm-password">
-                    <span class="material-symbols-outlined">
-                        lock_open
-                    </span>
-                    <button type="button">Change Password</button>
-                    <p>Don't have an account? <span> <u>Sign Up</u> </span></p>
-                </form>
+                <div class="form-container">
+                    <form action="" class="change-password">
+                        <h1>MAWMAW LIBRARY</h1>
+                        <h2>Please enter your new password</h2>
+                        <input type="password" name="password" id="forgot-password">
+                        <span class="material-symbols-outlined">
+                            lock_open
+                        </span>
+                        <input type="password" name="confirm-password" id="forgot-confirm-password">
+                        <span class="material-symbols-outlined">
+                            lock_open
+                        </span>
+                        <button type="button">Change Password</button>
+                        <p>Don't have an account? <a href="#register-container">Click Here</a></p>
+                    </form>
+                </div>
             </div>
 
 
